4c16d23a2d48cc0d9af05f5973b76e0ecf37c999
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2015-08-07 11:23:13 -0400

    Initializing milestone 4 V1.0

---
+++ CustomMade.h
@@ -0,0 +1,18 @@
+#ifndef __244_CUSTOMMADE_H__
+#define __244_CUSTOMMADE_H__
+ 
+
+
+namespace oop244{
+  class CustomMade:public .... {
+  private:
+
+
+  public:
+
+
+  };
+}
+
+
+#endif
---
+++ OnShelf.h
@@ -0,0 +1,15 @@
+#ifndef __244_OnShelf_H__
+#define __244_OnShelf_H__
+
+
+namespace oop244{
+  class OnShelf : public ...{
+  private:
+
+  public:
+
+
+
+  };
+}
+#endif
